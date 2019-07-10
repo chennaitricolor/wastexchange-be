f07e16c4a7845e19de2b2219f991e6630c261f71
VenkteshV <venkkratos@gmail.com>
2019-07-10 23:00:30 +0530

    update chennitricolo repo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const functions = require('firebase-functions');
 const app = require('./src/index')
 
-// // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
