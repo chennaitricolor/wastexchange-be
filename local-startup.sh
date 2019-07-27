0c5c8cde4648651aa7abe5d067e0cd7306fad0d7
Arunvel Sriram <arunvelsriram@gmail.com>
2019-07-28 00:22:40 +0530

    Fix local app startup script for mac/linux

--- local-startup.sh
+++ local-startup.sh
@@ -1,7 +1,11 @@
+export AUTH_KEY=abcd
 export PORT=7000
-export NODE_CONFIG_ENV='dev'
+export NODE_ENV=dev
+export NODE_CONFIG_ENV=dev
 export DB_HOST=t4c-waste-management.crnuwy9ruuzs.us-east-2.rds.amazonaws.com
 export DB_PORT=5432
 export DB_NAME=t4c_waster_management
-export DB_PWD=
-export DB_USER=
+export DB_PWD=dev
+export DB_USER=dev
+
+npm start
