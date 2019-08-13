ce452481e041ed7529408e5eaa028b88e079664c
Arunvel Sriram <arunvelsriram@gmail.com>
2019-08-13 22:12:26 +0530

    Revert "Seed the DB optionally"
    This reverts commit 24a0e7dfb103b8b4dc0d3b05670b155c8aeb53a8.

--- run.sh
+++ run.sh
@@ -4,7 +4,4 @@ set -e
 set -o pipefail
 
 dockerize -timeout 20s -wait tcp://${DB_HOST}:${DB_PORT} npx sequelize-cli db:migrate
-if [ "${SEED_DB}" = "true" ]; then
-  dockerize -timeout 20s -wait tcp://${DB_HOST}:${DB_PORT} npx sequelize-cli db:seed:all
-fi
 npm start
